77c6d21ff8ce596ab73448095ea3cd9a11df2f74
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-03-03 23:19:04 +0200

    added search query and simplified functions by JPA // doing good

--- src/main/java/com/group/InternMap/Job/JobPostingService.java
+++ src/main/java/com/group/InternMap/Job/JobPostingService.java
@@ -1,77 +1,32 @@
 package com.group.InternMap.Job;
-
 import com.group.InternMap.Deprecated.Repository.RepositoryAccessors;
 import org.springframework.stereotype.Service;
 import java.util.*;
-import java.util.stream.Collectors;
-import static com.group.InternMap.Deprecated.Repository.RepositoryAccessors.allJobPostings;
+
 
 @Service
 public class JobPostingService {
  JobRepo jobRepo;
 
     private final ArrayList<JobPosting> jobRepoo = RepositoryAccessors.allJobPostings;
-    public List<JobPosting> searchJobPostings(String searchQuery) throws Exception {
-        return jobRepoo.stream().filter(job -> {
-            boolean matches = false;
-
-            if (searchQuery != null && !searchQuery.isBlank()) {
-                matches |= job.getJobName().toLowerCase(Locale.ROOT).contains(searchQuery.toLowerCase(Locale.ROOT));
-                matches |= job.getCompany().getName().toLowerCase(Locale.ROOT).contains(searchQuery.toLowerCase(Locale.ROOT));
-            }
-
-            return matches;
-        }).toList();
-    }
-
-    public List<JobPosting> getAllJobPostings() throws Exception {
-        return  RepositoryAccessors.allJobPostings;
-    }
-    public JobPosting findByID(UUID jopPostingId) {
-        System.out.println(jopPostingId);
-
-        return allJobPostings.stream()
-//                 .filter(j -> j.getJobPostingUUID().equals(jopPostingId))
-                .findFirst().orElse(null);
-    }
-
-    public List<JobPosting> getJobPostingsByRecruiterId(long recruiterId) throws Exception {
-
-        System.out.println("Looking for jobs for recruiter: " + recruiterId);
-
-        return allJobPostings.stream()
-                .peek(job -> System.out.println("Job Recruiter: " +
-                        (job.getRecruiter() == null ? "null" : job.getRecruiter().getId())))
-                .filter(job -> job.getRecruiter() != null)
-                .filter(job -> recruiterId == job.getRecruiter().getId())
-                .collect(Collectors.toList());
-    }
-//
-//    public List<JobPosting> findJobpostringByname(String name) throws Exception {
-////        return RepositoryAccessors.allJobPostings.stream().filter(job -> job.getJobName().equalsIgnoreCase(name)).collect(Collectors.toList());
-//        List<JobPosting> jobPostingList = jobRepo.findAll();
-//        List<JobPosting> returnedJobs = new ArrayList<>();
-//
-//        for(JobPosting jobPosting : jobPostingList){
-//            if(jobPosting.getJobName().contains(name)){
-//                returnedJobs.add(jobPosting);
-//            }
-//        }
-//        if(returnedJobs.isEmpty()){
-//            throw new Exception("There is no Job with this specification");
-//        }
-//
-//        return returnedJobs;
-//
+//    public List<JobPosting> searchJobPostings(String searchQuery)  {
 //    }
 
-    public List<JobPosting> getAllJobPostingsName(String name) throws Exception {
+    public List<JobPosting> getAllJobPostings()  {
+        return  RepositoryAccessors.allJobPostings;
+    }
+
+    public List<JobPosting> getJobPostingsByRecruiterId(long recruiterId)  {
+        List<JobPosting> jobPostings = jobRepo.findJobPostingByRecruiterId(recruiterId);
+        return jobPostings;
+    }
+
+    public List<JobPosting> getAllJobPostingsName(String name) {
         return  jobRepo.findJobPostingByName(name);
     }
 
     public List<JobPosting> findJobpostingByID(long appId) {
         return  jobRepo.findJobPostingById(appId);
-
     }
 
 }
--- src/main/java/com/group/InternMap/Job/JobRepo.java
+++ src/main/java/com/group/InternMap/Job/JobRepo.java
@@ -3,6 +3,8 @@ package com.group.InternMap.Job;
 import com.group.InternMap.Student.Student;
 import org.springframework.data.domain.Limit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;

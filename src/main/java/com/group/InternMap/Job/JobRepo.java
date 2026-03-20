d24502f8a9cae85247a81679de520db7c8d110b0
shimaakamalm2-cmd <shimaakamalm2@gmail.com>
2026-03-20 23:22:39 +0200

    added a search query to job repo

--- src/main/java/com/group/InternMap/Job/JobRepo.java
+++ src/main/java/com/group/InternMap/Job/JobRepo.java
@@ -1,29 +1,23 @@
 package com.group.InternMap.Job;
-import com.group.InternMap.Application.Application;
-import jakarta.persistence.metamodel.SingularAttribute;
-import org.springframework.data.jpa.domain.AbstractPersistable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-
-import java.io.Serializable;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
 public interface JobRepo extends JpaRepository<JobPosting, Long> {
     List<JobPosting> findJobPostingByJobName(String jobName);
-
     @Override
-    Optional<JobPosting> findById(Long aLong);
+    Optional<JobPosting> findById(Long Long);
 
     JobPosting findJobPostingById(Long id);
 
     List<JobPosting> findJobPostingByRecruiterId(Long recruiterId);
 
+    @Query("SELECT j FROM JobPosting j JOIN  j.company c WHERE " +
+            "LOWER(j.jobName) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(c.name) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<JobPosting> searchJobs(@Param("query") String query);
 
-
-//    @Query("SELECT j FROM JobPosting j WHERE " +
-//            "LOWER(j.jobName) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
-//            "LOWER(j.jobDescription) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
-//            "LOWER(j.company.name) LIKE LOWER(CONCAT('%', :query, '%'))")
-//    List<JobPosting> searchJobs(@Param("query") String query);
 }
